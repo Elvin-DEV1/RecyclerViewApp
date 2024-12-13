a12f489577147b85d9d493f764098895b00a39cc
long <longmatic1.0@gmail.com>
2024-12-13 21:55:36 +0700

    commit gradle

--- RecyclerView/app/build.gradle.kts
+++ RecyclerView/app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
