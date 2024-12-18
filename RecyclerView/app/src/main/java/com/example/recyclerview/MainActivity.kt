a81393d856e6128f4f7c8bb1c0f18334d17de1a3
long <longmatic1.0@gmail.com>
2024-12-18 10:41:54 +0700

    Staggered grid layout manager + cardview

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -3,18 +3,69 @@ package com.example.recyclerview
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.recyclerview.User.User
+import com.example.recyclerview.User.UserAdapter
+import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUserLRecycler(getListUser())
+    }
+
+    private fun getListUser(): MutableList<User> {
+        val list = mutableListOf<User>()
+        list.add(User(R.drawable.anh_1, "anh 1"))
+        list.add(User(R.drawable.anh_2, "anh 2"))
+        list.add(User(R.drawable.anh_3, "anh 3"))
+        list.add(User(R.drawable.co_goc_manutd, "co goc manutd"))
+        list.add(User(R.drawable.manutd, "manutd"))
+        list.add(User(R.drawable.anh_manutd, "anh manutd"))
+        list.add(User(R.drawable.anh_1, "anh 1"))
+        list.add(User(R.drawable.anh_2, "anh 2"))
+        list.add(User(R.drawable.anh_3, "anh 3"))
+        list.add(User(R.drawable.co_goc_manutd, "co goc manutd"))
+        list.add(User(R.drawable.manutd, "manutd"))
+        list.add(User(R.drawable.anh_manutd, "anh manutd"))
+        list.add(User(R.drawable.anh_1, "anh 1"))
+        list.add(User(R.drawable.anh_2, "anh 2"))
+        list.add(User(R.drawable.anh_3, "anh 3"))
+        list.add(User(R.drawable.co_goc_manutd, "co goc manutd"))
+        list.add(User(R.drawable.manutd, "manutd"))
+        list.add(User(R.drawable.anh_manutd, "anh manutd"))
+        list.add(User(R.drawable.anh_1, "anh 1"))
+        list.add(User(R.drawable.anh_2, "anh 2"))
+        list.add(User(R.drawable.anh_3, "anh 3"))
+        list.add(User(R.drawable.co_goc_manutd, "co goc manutd"))
+        list.add(User(R.drawable.manutd, "manutd"))
+        list.add(User(R.drawable.anh_manutd, "anh manutd"))
+        list.add(User(R.drawable.anh_1, "anh 1"))
+        list.add(User(R.drawable.anh_2, "anh 2"))
+        list.add(User(R.drawable.anh_3, "anh 3"))
+        list.add(User(R.drawable.co_goc_manutd, "co goc manutd"))
+        list.add(User(R.drawable.manutd, "manutd"))
+        list.add(User(R.drawable.anh_manutd, "anh manutd"))
+        list.add(User(R.drawable.anh_1, "anh 1"))
+        list.add(User(R.drawable.anh_2, "anh 2"))
+        list.add(User(R.drawable.anh_3, "anh 3"))
+        list.add(User(R.drawable.co_goc_manutd, "co goc manutd"))
+        list.add(User(R.drawable.manutd, "manutd"))
+        list.add(User(R.drawable.anh_manutd, "anh manutd"))
+        return list
+    }
+
+    private fun setupUserLRecycler(list: MutableList<User>) {
+        userAdapter = UserAdapter(list)
+        binding.rcvUser.apply {
+            adapter = userAdapter
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         }
     }
 }
---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/User/User.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerview.User
+
+data class User(
+    val resourceImage: Int,
+    val name: String
+)
